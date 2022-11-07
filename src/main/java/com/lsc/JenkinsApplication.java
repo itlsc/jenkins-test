63d1bae31e523919cd9deaf2e95c85b66cb88b02
itlsc <1756332154@qq.com>
2022-11-07 14:01:25 +0800

    初始项目提交

---
+++ src/main/java/com/lsc/JenkinsApplication.java
@@ -0,0 +1,23 @@
+package com.lsc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lsc
+ * @version 1.0.0
+ * @date 2022/11/7 13:50
+ * @Description
+ */
+
+@SpringBootApplication
+@Slf4j
+public class JenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsApplication.class, args);
+    }
+
+
+}
---
+++ src/main/java/com/lsc/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.lsc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lsc
+ * @version 1.0.0
+ * @date 2022/11/7 13:53
+ * @Description
+ */
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/test")
+    public String testJenkins() {
+        return "测试jenkins服务";
+    }
+
+
+}
