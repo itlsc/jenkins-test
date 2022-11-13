40f3f0b7b534e64a05230a006e50f42e20bdfac0
itlsc <1756332154@qq.com>
2022-11-13 23:02:55 +0800

    测试

--- src/main/java/com/lsc/controller/TestController.java
+++ src/main/java/com/lsc/controller/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
 
     @RequestMapping(value = "/test")
     public String testJenkins() {
-        return "测试jenkins服务111";
+        return "测试jenkins服务333";
     }
 
 
